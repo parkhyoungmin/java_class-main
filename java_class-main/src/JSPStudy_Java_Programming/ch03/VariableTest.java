b9cf48f1f84c4dd84c7e8b7772b50ad2a24e5b3b
parkhyoungmin <parkhm94@naver.com>
2021-06-27 14:45:40 +0900

    정리

---
+++ java_class-main/src/JSPStudy_Java_Programming/ch03/VariableTest.java
@@ -0,0 +1,13 @@
+package JSPStudy_Java_Programming.ch03;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		String programming;
+		programming = "Java";
+		String name = "park";
+		
+		System.out.println("내가 배우는 과목: " + programming);
+		System.out.println("나는: " + name);
+	}
+}
---
+++ java_class-main/src/JSPStudy_Java_Programming/readme.java
@@ -0,0 +1,9 @@
+package JSPStudy_Java_Programming;
+
+public class readme {
+
+	public static void main(String[] args) {
+		System.out.println("JSPStudy_Java_Programming공부");
+	}
+	
+}
