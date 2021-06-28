31eedf10173ad35d3fa211a4d3eec07e7653adae
parkhyoungmin <parkhm94@naver.com>
2021-06-29 08:20:15 +0900

    달력

--- java_class-main/src/java_playground/Calendar/Calendar00.java
+++ java_class-main/src/java_playground/Calendar/Calendar00.java
@@ -22,18 +22,39 @@ public class Calendar00 {
 	public static void main(String[] args) {
 		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		Scanner scanner = new Scanner(System.in);
+//		Calendar00 cal = new Calendar00();
+//		
+//		System.out.println("반복횟수를 입력해주세요.");
+//		int repeat = scanner.nextInt();
+//		
+//		for (int i = 0; i < repeat; i++) {
+//			System.out.println("월을 입력해주세요.");
+//			int month = scanner.nextInt();
+//			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+//		}
+//		System.out.println("입력종료");
+//		scanner.close();
+		
+		String prompt = "cal> ";
 		Calendar00 cal = new Calendar00();
 		
-		System.out.println("반복횟수를 입력해주세요.");
-		int repeat = scanner.nextInt();
+		int month = 1;
 		
-		for (int i = 0; i < repeat; i++) {
-			System.out.println("월을 입력해주세요.");
-			int month = scanner.nextInt();
+		while (true) {
+			System.out.println("달을 입력하세요");
+			System.out.println(prompt);
+			month = scanner.nextInt();
+			if (month == -1) {
+				break;
+			}
+			if (month > 12) {
+				continue;
+			}
 			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
 		}
 		System.out.println("입력종료");
 		scanner.close();
+		
 	}
 
 }
