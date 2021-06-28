9560a681ec6df4b5518a593d3cc92d8c565b465a
parkhyoungmin <parkhm94@naver.com>
2021-06-29 07:29:25 +0900

    달력

--- java_class-main/src/java_playground/Calendar/Calendar00.java
+++ java_class-main/src/java_playground/Calendar/Calendar00.java
@@ -1,8 +1,16 @@
 package java_playground.Calendar;
 
-public class Calendar00 {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
+public class Calendar00 {
+	
+	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1];
+	}
+	
+	public void printSampleCalendar() {
 		System.out.println("일 월 화 수 목 금 토");
 		System.out.println("--------------------");
 		System.out.println("1  2  3  4  5  6  7");
@@ -11,4 +19,42 @@ public class Calendar00 {
 		System.out.println("22 23 24 25 26 27 28");
 	}
 
+	public static void main(String[] args) {
+		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		Scanner scanner = new Scanner(System.in);
+		Calendar00 cal = new Calendar00();
+		System.out.println("월을 입력해주세요.");
+		int month = scanner.nextInt();
+		
+//		1.
+//		if (month < 1 || month > 12) {
+//			System.out.println("잘못입력하셨습니다.");
+//		} else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+//			System.out.println("31일입니다.");
+//		} else if (month == 4 || month == 6 || month == 9 || month == 11) {
+//			System.out.println("30일입니다.");
+//		} else {
+//			System.out.println("28일 혹은 29일입니다.");
+//		}
+		
+//		2.
+		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		
+		System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+		scanner.close();
+	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
