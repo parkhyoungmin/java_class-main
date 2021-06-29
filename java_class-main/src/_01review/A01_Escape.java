3f1e904848cd8bfb30f6aae2bb0c57436e4db545
parkhyoungmin <parkhm94@naver.com>
2021-06-30 03:50:22 +0900

    복습 패캐지 생성, A부분복습

---
+++ java_class-main/src/_01review/A01_Escape.java
@@ -0,0 +1,19 @@
+package _01review;
+
+public class A01_Escape {
+
+	public static void main(String[] args) {
+		System.out.println("Hello\nworld!");
+//		Hello
+//		world!
+		System.out.println("안\t녕");
+//		안	녕
+		System.out.println("한\\영");
+//		한\영
+		System.out.println("\"큰따옴표\"");
+//		"큰따옴표"
+		System.out.println("\'작은따옴표\'");
+//		'작은따옴표'
+	}
+	
+}
