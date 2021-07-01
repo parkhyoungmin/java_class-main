62a512cb9f4cfe8e3cd48f9900632a65a21da846
parkhyoungmin <parkhm94@naver.com>
2021-07-01 19:37:35 +0900

    B02

--- java_class-main/src/B02_operator02.java
+++ java_class-main/src/B02_operator02.java
@@ -7,13 +7,13 @@ public class B02_operator02 {
 		//	- 산술 연산과 비교 연산이 함께 있으면 산술 연산을 먼저 계산한다
 		int a = 5, b = 9;
 		
-		System.out.println("a > b : " + (a > b));
-		System.out.println("a < b : " + (a < b));
-		System.out.println("a >= b : " + (a >= b));
-		System.out.println("a <= b : " + (a <= b));
+		System.out.println("a > b : " + (a > b)); //false
+		System.out.println("a < b : " + (a < b)); //true
+		System.out.println("a >= b : " + (a >= b)); //false
+		System.out.println("a <= b : " + (a <= b)); //true
 		
-		System.out.println("a == b : " + (a == b)); // 같으면 true
-		System.out.println("a != b : " + (a != b)); // 다르면 true
+		System.out.println("a == b : " + (a == b)); // 같으면 true //false
+		System.out.println("a != b : " + (a != b)); // 다르면 true //true
 		
 		// # 논리 연산
 		//	- boolean 값으로 하는 연산
@@ -21,21 +21,20 @@ public class B02_operator02 {
 		//	- || : 두 값 중 하나만 true여도 true (OR)
 		//	- !  : true면 false, false면 true (NOT)
 		System.out.println("#### AND ####");
-		System.out.println(true && true);
-		System.out.println(true && false);
-		System.out.println(false && true);
-		System.out.println(false && false);
+		System.out.println(true && true); //true
+		System.out.println(true && false); //false
+		System.out.println(false && true); //false
+		System.out.println(false && false); //false
 		
 		System.out.println("#### OR ####");
-		System.out.println(true || true);
-		System.out.println(true || false);
-		System.out.println(false || true);
-		System.out.println(false || false);
+		System.out.println(true || true); //true
+		System.out.println(true || false); //true
+		System.out.println(false || true); //true
+		System.out.println(false || false); //false
 		
 		System.out.println("#### NOT ####");
-		System.out.println(!true);
-		System.out.println(!false);
-		System.out.println(!(a < 10));
+		System.out.println(!true); //false
+		System.out.println(!false); //true
 	}
 	
 }
