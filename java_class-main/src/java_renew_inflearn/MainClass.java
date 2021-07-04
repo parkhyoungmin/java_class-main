e022fad859c8dd68574c0764c0e04d9c3ddc34f9
parkhyoungmin <parkhm94@naver.com>
2021-07-05 01:53:25 +0900

    객체부분

---
+++ java_class-main/src/java_renew_inflearn/MainClass.java
@@ -0,0 +1,11 @@
+package java_renew_inflearn;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		ObjectClass obj = new ObjectClass(10, 20);
+
+	}
+
+}
---
+++ java_class-main/src/java_renew_inflearn/Student.java
@@ -0,0 +1,22 @@
+package java_renew_inflearn;
+
+public class Student {
+
+	private String name;
+	private int score;
+	
+	// 단축키 : Alt + Shift + s키
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+}
