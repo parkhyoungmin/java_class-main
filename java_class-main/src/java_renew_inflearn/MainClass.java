d6e28433a07f2e81a4021ccdd9bf684c0959a026
parkhyoungmin <parkhm94@naver.com>
2021-07-15 19:26:40 +0900

    B10까지복습

--- java_class-main/src/java_renew_inflearn/MainClass.java
+++ java_class-main/src/java_renew_inflearn/MainClass.java
@@ -4,27 +4,15 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		
-//		String str = "JAVA";
-		String str = new String("JAVA");
-		System.out.println("str : " + str);
-		
-		str = str + "_JAVA";
-		System.out.println("str : " + str);
-		
-		//StringBuffer
-		StringBuffer sf = new StringBuffer("JAVA");
-		System.out.println("sf : " + sf);
-		sf.append("world"); // 문자열 추가
-		System.out.println("sf : " + sf);
-		System.out.println("sf.length() : " + sf.length());
-		sf.insert(sf.length(), "~~~~"); // 문자열끝에 추가하겠다.
-		System.out.println("sf : " + sf);
-		
-		sf.delete(4, 9); // 4부터 8위치까지 삭제
-		System.out.println("sf : " + sf);
-		
-		StringBuffer sb = new StringBuffer("HelloWorld");
-		System.out.println("sb : " + sb);
+		for (int i = 0; i < 10; ++i) {
+			System.out.printf("===========================바깥 반복문 %d\n", i);
+			for (int j = 0; j < 5; ++j) {
+				System.out.printf("--------------------안쪽 반복문 %d-%d\n", i, j);				
+				for (int k = 0; k < 2; ++k) {
+					System.out.printf("+++++++++제일 안쪽 반복문 %d-%d-%d\n", i, j, k);
+				}				
+			}			
+		}
 		
 	}
 
