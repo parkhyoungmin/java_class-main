da5a2c0b5d6945a1c157134589314970aeb039ee
parkhyoungmin <parkhm94@naver.com>
2021-07-27 01:09:01 +0900

    별 역피라미드 출력문제 연습

--- java_class-main/src/_01review/Bird.java
+++
@@ -1,8 +0,0 @@
-package _01review;
-
-public abstract class Bird { // 추상클래스 생성
-	public abstract void sing(); // 추상메서드
-	public void fly() {
-		
-	}
-}
---
+++ java_class-main/src/_01review/reverseStar.java
@@ -0,0 +1,19 @@
+package _01review;
+
+public class reverseStar {
+
+	public static void main(String[] args) {
+//		1. "*" 출력(역피라미드로 출력)
+		int num = 5;
+		for (int i = num; i > 0; i--) {
+			for (int j = num - i; j > 0; j--) {
+				System.out.print(" ");
+			}
+			for (int k = i * 2 - 1; k > 0; k--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	
+}
