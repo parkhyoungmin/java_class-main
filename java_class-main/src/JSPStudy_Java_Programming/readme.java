a57e1379cf2feb91c547d6b362882c2b15868852
parkhyoungmin <parkhm94@naver.com>
2021-08-05 03:27:32 +0900

    .

--- java_class-main/src/JSPStudy_Java_Programming/readme.java
+++
@@ -1,9 +0,0 @@
-package JSPStudy_Java_Programming;
-
-public class readme {
-
-	public static void main(String[] args) {
-		System.out.println("JSPStudy_Java_Programming공부");
-	}
-	
-}
--- java_class-main/src/java_01/Type.java
+++
@@ -1,5 +0,0 @@
-package java_01;
-
-public enum Type {
-	WALKING, RUNING, TRACKING, HICKING
-}
