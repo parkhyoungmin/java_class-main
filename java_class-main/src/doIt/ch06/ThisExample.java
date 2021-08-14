4e1b4f3543e078b8d03ac5748992b5884a999567
parkhyoungmin <parkhm94@naver.com>
2021-08-14 11:25:42 +0900

    06-2

---
+++ java_class-main/src/doIt/ch06/ThisExample.java
@@ -0,0 +1,20 @@
+package doIt.ch06;
+
+class Birthday {
+	int day;
+	int month;
+	int year;
+	
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+}
+
+public class ThisExample {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
