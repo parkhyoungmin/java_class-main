5065f76876dcf8d61b8eed8c3cf39602633b68a0
parkhyoungmin <parkhm94@naver.com>
2021-08-14 13:03:23 +0900

    static

---
+++ java_class-main/src/doIt/ch06/Student3.java
@@ -0,0 +1,34 @@
+package doIt.ch06;
+
+public class Student3 {
+
+	private static int serialNum = 1000;
+	int studentID;
+	String studentName;
+	int grade;
+	String address;
+	int cardNumber;
+	
+	public Student3(){
+		serialNum++;
+		studentID = serialNum;
+		cardNumber = studentID + 100;
+	}
+	
+	public String getStudentName(){
+		return studentName;
+	}
+	
+	public void setStudentName(String name){
+		studentName = name;
+	}
+
+	public static int getSerialNum() {
+		return serialNum;
+	}
+
+	public static void setSerialNum(int serialNum) {
+		Student3.serialNum = serialNum;
+	}
+	
+}
