4850a8e3c2ee1f178d8c6d071737cccc28bcf130
parkhyoungmin <parkhm94@naver.com>
2021-08-20 15:21:49 +0900

    ArrayList

---
+++ java_class-main/src/doIt/ch07/ArrayTest.java
@@ -0,0 +1,11 @@
+package doIt.ch07;
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+		
+		int[] numbers = new int[3];
+		
+	}
+
+}
---
+++ java_class-main/src/doIt/ch07/EnhancedForLoop.java
@@ -0,0 +1,15 @@
+package doIt.ch07;
+
+public class EnhancedForLoop {
+
+	public static void main(String[] args) {
+
+		String[] strArr = {"Java", "Android", "C"};
+		
+		for (String lang : strArr) {
+			System.out.println(lang);
+		}
+		
+	}
+
+}
